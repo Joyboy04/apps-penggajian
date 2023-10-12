6c823dd9c48622944e0ba825c028695f39129263
Raihan Revi Kasya <raihanrevikasya12@gmail.com>
2023-10-12 15:37:13 +0700

    database

---
+++ Database/penggajian.sql
@@ -0,0 +1,217 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 12 Okt 2023 pada 10.35
+-- Versi server: 10.4.28-MariaDB
+-- Versi PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `penggajian`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_jabatan`
+--
+
+CREATE TABLE `data_jabatan` (
+  `id_jabatan` int(11) NOT NULL,
+  `nama_jabatan` varchar(120) NOT NULL,
+  `gaji_pokok` varchar(50) NOT NULL,
+  `tj_transport` varchar(50) NOT NULL,
+  `uang_makan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `data_jabatan`
+--
+
+INSERT INTO `data_jabatan` (`id_jabatan`, `nama_jabatan`, `gaji_pokok`, `tj_transport`, `uang_makan`) VALUES
+(1, 'Staff Marketing', '4000000', '800000', '500000'),
+(4, 'IT ', '5000000', '800000', '500000'),
+(5, 'Software Engineer', '6500000', '800000', '700000'),
+(6, 'Admin', '4000000', '800000', '500000');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_kehadiran`
+--
+
+CREATE TABLE `data_kehadiran` (
+  `id_kehadiran` int(11) NOT NULL,
+  `bulan` varchar(15) NOT NULL,
+  `nik` varchar(50) NOT NULL,
+  `nama_pegawai` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(50) NOT NULL,
+  `nama_jabatan` varchar(50) NOT NULL,
+  `hadir` int(11) NOT NULL,
+  `sakit` int(11) NOT NULL,
+  `alpha` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `data_kehadiran`
+--
+
+INSERT INTO `data_kehadiran` (`id_kehadiran`, `bulan`, `nik`, `nama_pegawai`, `jenis_kelamin`, `nama_jabatan`, `hadir`, `sakit`, `alpha`) VALUES
+(21, '102023', '3175060402030006', 'Adnan Sayyid', 'Laki-Laki', 'Software Engineer', 1, 1, 1),
+(22, '102023', '123456789', 'Mahesa', 'Laki-Laki', 'IT ', 2, 2, 2),
+(23, '102023', '13456728745', 'Raihan Revi', 'Laki-Laki', 'Staff Marketing', 3, 3, 3),
+(24, '092023', '3175060402030006', 'Adnan Sayyid', 'Laki-Laki', 'Software Engineer', 22, 0, 0),
+(25, '092023', '123456789', 'Mahesa', 'Laki-Laki', 'IT ', 20, 1, 3),
+(26, '092023', '13456728745', 'Raihan Revi', 'Laki-Laki', 'Staff Marketing', 21, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `data_pegawai`
+--
+
+CREATE TABLE `data_pegawai` (
+  `id_pegawai` int(11) NOT NULL,
+  `nik` varchar(50) NOT NULL,
+  `nama_pegawai` varchar(225) NOT NULL,
+  `username` varchar(120) NOT NULL,
+  `password` varchar(120) NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL,
+  `jabatan` varchar(50) NOT NULL,
+  `tanggal_masuk` date NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `photo` varchar(225) NOT NULL,
+  `hak_akses` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `data_pegawai`
+--
+
+INSERT INTO `data_pegawai` (`id_pegawai`, `nik`, `nama_pegawai`, `username`, `password`, `jenis_kelamin`, `jabatan`, `tanggal_masuk`, `status`, `photo`, `hak_akses`) VALUES
+(19, '3175060402030006', 'Adnan Sayyid', 'adnan', '202cb962ac59075b964b07152d234b70', 'Laki-Laki', 'Software Engineer', '2023-10-10', 'Karyawan Tetap', 'Adnan1.png', 1),
+(20, '123456789', 'Mahesa', 'esa', '827ccb0eea8a706c4c34a16891f84e7b', 'Laki-Laki', 'IT ', '2023-10-10', 'Karyawan Kontrak', 'Mahesa2.png', 2),
+(21, '13456728745', 'Raihan Revi', 'revi', 'e10adc3949ba59abbe56e057f20f883e', 'Laki-Laki', 'Staff Marketing', '2023-10-10', 'Karyawan Kontrak', 'Raihan1.png', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `hak_akses`
+--
+
+CREATE TABLE `hak_akses` (
+  `id` int(11) NOT NULL,
+  `keterangan` varchar(50) NOT NULL,
+  `hak_akses` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `hak_akses`
+--
+
+INSERT INTO `hak_akses` (`id`, `keterangan`, `hak_akses`) VALUES
+(1, 'Admin', 1),
+(2, 'Pegawai', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `potongan_gaji`
+--
+
+CREATE TABLE `potongan_gaji` (
+  `id` int(11) NOT NULL,
+  `potongan` varchar(120) NOT NULL,
+  `jml_potongan` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `potongan_gaji`
+--
+
+INSERT INTO `potongan_gaji` (`id`, `potongan`, `jml_potongan`) VALUES
+(1, 'Alpha', 50000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `data_jabatan`
+--
+ALTER TABLE `data_jabatan`
+  ADD PRIMARY KEY (`id_jabatan`);
+
+--
+-- Indeks untuk tabel `data_kehadiran`
+--
+ALTER TABLE `data_kehadiran`
+  ADD PRIMARY KEY (`id_kehadiran`);
+
+--
+-- Indeks untuk tabel `data_pegawai`
+--
+ALTER TABLE `data_pegawai`
+  ADD PRIMARY KEY (`id_pegawai`);
+
+--
+-- Indeks untuk tabel `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `potongan_gaji`
+--
+ALTER TABLE `potongan_gaji`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `data_jabatan`
+--
+ALTER TABLE `data_jabatan`
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `data_kehadiran`
+--
+ALTER TABLE `data_kehadiran`
+  MODIFY `id_kehadiran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT untuk tabel `data_pegawai`
+--
+ALTER TABLE `data_pegawai`
+  MODIFY `id_pegawai` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT untuk tabel `hak_akses`
+--
+ALTER TABLE `hak_akses`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `potongan_gaji`
+--
+ALTER TABLE `potongan_gaji`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
